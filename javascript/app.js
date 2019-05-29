98b130f385cebf4bd4051e8c54aa6f749236ac4c
Diana <dianavcarrara@gmail.com>
2019-05-28 18:28:42 -0600

    added JS + modal

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+var modal = document.getElementById("myModal");
+
+// var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+
+$('.myImg').on('click', function() {
+    
+    modal.style.display = "block";
+    modalImg.src = this.src;
+     
+});
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+    modal.style.display = "none";
+}
